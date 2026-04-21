486dcc2ece9f11494d9bae299449ca898c915e9e
LevilSon2005 <bbromero@pucp.edu.pe>
2026-04-21 04:33:33 -0500

    modulo de producto

---
+++ Back/Shiligama/Persistance/src/main/java/pe/edu/pucp/persistance/dao/producto/dao/CategoriaDAO.java
@@ -0,0 +1,8 @@
+package pe.edu.pucp.persistance.dao.producto.dao;
+
+import pe.edu.pucp.model.producto.CategoriaDto;
+import pe.edu.pucp.persistance.dao.dao.IDAO;
+
+public interface CategoriaDAO extends IDAO<CategoriaDto> {
+
+}
---
+++ Back/Shiligama/Persistance/src/main/java/pe/edu/pucp/persistance/dao/producto/dao/ProductoDAO.java
@@ -0,0 +1,8 @@
+package pe.edu.pucp.persistance.dao.producto.dao;
+
+import pe.edu.pucp.model.producto.ProductoDto;
+import pe.edu.pucp.persistance.dao.dao.IDAO;
+
+public interface ProductoDAO extends IDAO<ProductoDto> {
+
+}
