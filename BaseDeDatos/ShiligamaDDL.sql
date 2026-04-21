1bf943834fae70bd0f9d20bdf37c60f8bd59454f
LuisMirandaMallqui <a20223796@pucp.edu.pe>
2026-04-21 11:04:46 -0500

    DDL limpio

--- BaseDeDatos/ShiligamaDDL.sql
+++ BaseDeDatos/ShiligamaDDL.sql
@@ -3,9 +3,6 @@
 -- DDL: Definicion de tablas (MySQL 8+)
 -- Equipo: Team Script - Programacion 3 (PUCP) 2026-1
 -- =====================================================================
--- SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
--- SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
--- SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 -- -----------------------------------------------------
 -- Schema shiligama
 -- -----------------------------------------------------
@@ -515,14 +512,6 @@ CREATE TABLE IF NOT EXISTS `detalles_orden_reabastecimiento` (
     CONSTRAINT `fk_det_orden_productos`
         FOREIGN KEY (`PRODUCTO_ID`) REFERENCES `productos` (`PRODUCTO_ID`) ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4;
-
--- =============================================================
--- RESTAURAR CONFIGURACION
--- =============================================================
--- SET SQL_MODE=@OLD_SQL_MODE;
--- SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
 -- =============================================================
 -- RESUMEN DE TABLAS
 -- =============================================================
