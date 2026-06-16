92bea180ca79132ad617f3d5cb629048fc3b5515
Enger <a20233642@pucp.edu.pe>
2026-06-15 19:50:01 -0500

    cambios temporales

---
+++ Front/Shiligama/shilligama-blazor/wwwroot/inputFilters.js
@@ -0,0 +1,13 @@
+﻿window.inputFilters = {
+    onlyDecimal: function (input) {
+        input.addEventListener("input", function () {
+            this.value = this.value.replace(/[^0-9.,]/g, '');
+        });
+    },
+
+    onlyInteger: function (input) {
+        input.addEventListener("input", function () {
+            this.value = this.value.replace(/[^0-9]/g, '');
+        });
+    }
+};
